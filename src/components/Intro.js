0b69c97d9fac83558b60bb477eb8cfcd429d9bd9
Miu Ha <wmh0226@gmail.com>
2023-07-30 17:42:43 -0400

    update ui

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,18 +6,23 @@ function Intro() {
   return (
     <div id="home" className={styles.container}>
       <div className={styles.introContainer}>
-        <div className={styles.introFirst}>Hi, my name is</div>
-        <div className={styles.introName}>Miu Ha Wong.</div>
-        <div className={styles.introBuild}>
-          <span className={styles.text}>I build things for the web</span>
+        <div className={styles.fadeIn}>
+          <div className={styles.introFirst}>Hi, my name is</div>
+          <div className={styles.introName}>Miu Ha Wong.</div>
         </div>
 
-        <div className={styles.introDescript}>
-          I'm a Toronto-based web developer skilled in HTML, CSS, JavaScript,
-          and React. Driven to create dynamic, user-friendly web apps and stay
-          current with tech trends. Let's collaborate!
+        <div className={styles.typewriter}>
+          <span className={styles.text}>Code with Passion!</span>
         </div>
-        <a href="mailto:wmh0226@gmail.com">
+        <div className={styles.fadeIn}>
+          <div className={styles.introDescript}>
+            I'm a Toronto-based web developer skilled in HTML, CSS, JavaScript,
+            and React. Driven to create dynamic, user-friendly web apps and stay
+            current with tech trends. Let's collaborate!
+          </div>
+        </div>
+
+        <a className={styles.btnFadeIn} href="mailto:wmh0226@gmail.com">
           <button className={styles.introButton}>Get In Touch</button>
         </a>
       </div>
